0c5ec93f64bd5dfe713d73d6251b8e4957f16c51
Oivar Micolta <oivarmicolta@gmail.com>
2022-11-07 20:53:43 -0500

    initial commit

---
+++ src/main/java/com/vefree/vefreeback/VefreeBackApplication.java
@@ -0,0 +1,13 @@
+package com.vefree.vefreeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VefreeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VefreeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vefree/vefreeback/VefreeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vefree.vefreeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VefreeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
